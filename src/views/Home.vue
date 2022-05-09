b3fdc00eef14308f36b45f5f2d6b125cfe18cf96
Isaiah Rappaport <irappap98@gmail.com>
2022-05-09 16:13:46 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <br>
+    <div v-for="song in songs">
+      <h3 class="title">Title: {{song.name}}</h3>
+      <h5 class="artist">By: {{song.artist}}</h5>
+    </div>
   </div>
 </template>
 
+<style>
+.artist {
+  font-weight: 200;
+}
+
+.by {
+  font-weight: 400;
+}
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Songwriter 2.0",
+      songs: [],
+    };
+    x;
+  },
+  created: function () {
+    this.indexSongs();
+  },
+  methods: {
+    indexSongs: function () {
+      axios.get("/api/songs").then((response) => {
+        console.log("songs", response.data);
+        this.songs = response.data;
+      });
+    },
+  },
+};
 </script>
+
